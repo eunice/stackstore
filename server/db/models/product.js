7ad9c4df4b981bdac61652ff0f011875da54312c
Robbie Ferguson <robbieferguson139@gmail.com>
2015-06-08 18:36:07 -0400

    models created and initial tests passing

---
+++ server/db/models/product.js
@@ -0,0 +1,33 @@
+'use strict';
+var mongoose = require('mongoose');
+
+var schema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    quantity: {
+        type: Number,
+        default: Infinity
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    img: {
+        type: String,
+        default: '/images/elon.png'
+    },
+    reviews: [{type: mongoose.Schema.Types.ObjectId, ref: 'Review'}]
+});
+
+
+mongoose.model('Product', schema);

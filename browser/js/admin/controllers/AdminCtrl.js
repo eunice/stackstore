b7df807f8c28eb7fbab57941c61c10a399ef7e8d
Eunice Lee <eueu.lee@gmail.com>
2015-06-16 13:32:45 -0400

    pw reset not working still

--- browser/js/admin/controllers/AdminCtrl.js
+++ browser/js/admin/controllers/AdminCtrl.js
@@ -1,44 +1,10 @@
-app.controller('AdminCtrl', function ($scope, AuthService, AdminFactory, $state, Session) {
+app.controller('AdminCtrl', function ($scope, Session, AuthService, AdminFactory, $state, Session) {
 
   // write the admin authen func
   (function isAdmin () {
 
-    AuthService.getLoggedInUser().then(function (user) {
-        $scope.user = user;
-        console.log($scope.user.userType)
-        if ($scope.user.userType === "Admin") $scope.isAdmin=true
-    });
+    if (Session.user.userType === "Admin") $scope.isAdmin=true
 
   })()
-
-
-    // $scope.signin = {userType: "User"};
-    // $scope.error = null;
-    // $scope.passwordMatch = false;
-
-    // $scope.sendSignin = function (loginInfo) {
-
-    //     if ($scope.signin.password !== $scope.passwordConfirm) {
-    //       console.log($scope.signin.password)
-    //       console.log($scope.passwordConfirm)
-    //         $scope.passwordMatch = true;
-    //         return;
-    //     }
-    //
-    //     UserInfo.checkSignUp(loginInfo.email).then (function(founduser) {
-    //       console.log('founduser', founduser);
-    //         if(founduser){
-    //             $scope.error = true;
-    //
-    //         }else{
-    //           console.log('signuppppp', loginInfo);
-    //             UserInfo.signUpInfo(loginInfo);
-    //
-    //         }
-    //     });
-    //
-    //     $scope.signin = {userType: "User"};
-    //     $scope.passwordConfirm = null;
-    // };
 
 });
--- browser/js/resetpw/resetpw.js
+++ browser/js/resetpw/resetpw.js
@@ -8,63 +8,33 @@ app.config(function ($stateProvider) {
 
 });
 
-app.controller('ResetPwCtrl', function ($scope, UserInfo, Session, AuthService, $state, $window, $location) {
+app.controller('ResetPwCtrl', function ($scope, UserInfo, $state, $window, $location) {
 
   $scope.error = null;
   $scope.passwordMatch = false;
 
-  $scope.pw = {}
+  $scope.newpw = {password: null}
 
-  $scope.resetPw = function (loginInfo) {
 
-      if ($scope.signin.password !== $scope.passwordConfirm) {
-        console.log($scope.signin.password)
-        console.log($scope.passwordConfirm)
+  $scope.resetPw = function (pwInfo) {
+
+      if ($scope.newpw.password !== $scope.passwordConfirm) {
+        console.log('newpw',$scope.newpw)
+        console.log('pwconfirm',$scope.passwordConfirm)
           $scope.passwordMatch = true;
           return;
       }
 
+      $scope.newpw.reset = false;
+      UserInfo.updatePw($scope.newpw)
+
       // UserInfo.checkSignUp(loginInfo.email).then (function(founduser) {
-      //   console.log('founduser', founduser);
-      //     if(founduser){
-      //         $scope.error = true;
-      //
-      //     }else{
-      //       console.log('signuppppp', loginInfo);
-      //         UserInfo.signUpInfo(loginInfo);
-      //
-      //     }
-      // });
+
 
       $scope.passwordConfirm = null;
 
-      Session.user.reset = false;
 
   };
 
-    // $scope.login = {};
-    // $scope.error = null;
-    //
-    // $scope.sendLogin = function (loginInfo) {
-    //
-    //     $scope.error = null;
-    //
-    //
-    //     AuthService.login(loginInfo).then(function () {
-    //         console.log('hello')
-    //
-    //         $state.go('home');
-    //         // $location.href="http://localhost:1337/discover"
-    //     }).catch(function () {
-    //         $scope.error = 'Invalid login credentials.';
-    //     });
-    //
-    // };
-    //
-    // $scope.redirectLogin = function(location){
-    //   console.log('oauth', location)
-    //   $window.location.href = "/auth/" + location;
-    //     // $state.go('discover');
-    // };
 
 });
